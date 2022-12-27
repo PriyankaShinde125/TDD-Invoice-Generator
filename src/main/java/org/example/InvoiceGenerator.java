992fffcf8e7876b356080e84f6a2eca35fcd59a4
PriyankaShinde125 <pkshinde125@gmail.com>
2022-12-27 09:43:17 +0530

    ADDED : Code to calculate fare

--- src/main/java/org/example/InvoiceGenerator.java
+++ src/main/java/org/example/InvoiceGenerator.java
@@ -1,7 +1,15 @@
 package org.example;
 
 public class InvoiceGenerator {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+    public static final int COST_PER_DISTANCE = 10;
+    public static final int COST_PER_MINUTE = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public double calculateFare(double distance, int time) {
+        return COST_PER_DISTANCE * distance + COST_PER_MINUTE * time;
     }
 }
--- src/test/java/org/example/InvoiceGeneratorTest.java
+++ src/test/java/org/example/InvoiceGeneratorTest.java
@@ -1,5 +1,14 @@
 package org.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class InvoiceGeneratorTest {
 
+    @Test
+    public void givenDistanceAndTime_shouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double totalFare = invoiceGenerator.calculateFare(2.5, 5);
+        Assert.assertEquals(30,totalFare,0.0);
+    }
 }
